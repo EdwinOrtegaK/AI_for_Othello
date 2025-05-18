0bad7e22221ea3659751a7264a135356ef5802bc
EdwinOrtegaK <andyortegakou@.com.mx>
2025-05-18 17:43:30 -0600

    test: add unit tests for validation of moves, moves and end of game

---
+++ tests/test_game_engine.py
@@ -0,0 +1,31 @@
+import unittest
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.game_engine.board import initial_board
+from src.game_engine.move_generator import valid_moves
+from src.game_engine.othello import apply_move, game_over, winner
+
+class TestGameEngine(unittest.TestCase):
+
+    def test_initial_moves(self):
+        board = initial_board()
+        # En la apertura, ambos tienen 4 jugadas posibles
+        self.assertEqual(len(valid_moves(board, 1)), 4)
+        self.assertEqual(len(valid_moves(board, -1)), 4)
+
+    def test_simple_play(self):
+        board = initial_board()
+        move = (2, 3)     # Negra juega arriba de la ficha inicial
+        board = apply_move(board, -1, move)
+        # Luego de la jugada, blancas tienen movimientos
+        self.assertTrue(len(valid_moves(board, 1)) > 0)
+
+    def test_end_game(self):
+        board = initial_board()
+        # Forzamos fin (rellenamos todo de blancas)
+        board = [[1]*8 for _ in range(8)]
+        self.assertTrue(game_over(board))
+        self.assertEqual(winner(board), 1)
+
+if __name__ == "__main__":
+    unittest.main()
